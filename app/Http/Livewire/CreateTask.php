35cedee372380a36332001b97e9f265b37262cf5
m-ermolaev <lainiwakurafan@gmail.com>
2023-04-19 23:44:12 +0800

    Deny duplicated tasks

--- app/Http/Livewire/CreateTask.php
+++ app/Http/Livewire/CreateTask.php
@@ -9,12 +9,22 @@ class CreateTask extends Component
 {
     public $name;
 
+    public function handleNameChange()
+    {
+        $this->resetValidation('name');
+    }
+
     public function handleCreateClick()
     {
-        Task::create(['name'=>$this->name]);
+        $nameExist = Task::where('name', $this->name)->count() > 0;
+        if (!$nameExist) {
+            Task::create(['name' => $this->name]);
 
-        $this->name = '';
-        $this->emit('refreshList');
+            $this->name = '';
+            $this->emit('refreshList');
+        } else {
+            $this->addError('name', 'Task with this name already exists in the list.');
+        }
     }
 
     public function render()
